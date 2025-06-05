fb601269a26b219f7ddb7f9a1b02747e65383f41
Lee jae heon (pc) <thomaslee330@ajou.ac.kr>
2025-06-05 17:37:02 +0900

    Success API version

--- back/src/main/java/com/example/graduationcheck/GraduationCheckApplication.java
+++ back/src/main/java/com/example/graduationcheck/GraduationCheckApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GraduationCheckApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GraduationCheckApplication.class, args);
+		System.out.println("✅ 서버 실행 성공");
 	}
 }
